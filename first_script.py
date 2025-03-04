0350b305136b6b7958203bfca1615b40e0486341
JMartindale612 <74535235+JMartindale612@users.noreply.github.com>
2025-03-04 14:19:13 +0000

    Started some basic scoring

--- first_script.py
+++ first_script.py
@@ -3,33 +3,19 @@
 import pandas as pd
 import numpy as np
 
-a = 5
-a
-
-print("Hello world!")
-
-data = [np.random.standard_normal() for i in range(7)]
-
-b = "another way of writing a string"
-
-x = -5
-
-if x < 0:
-    print("It's negative!")
-
-## Data structures -----
-
-tup = tuple([4, 0, 2])
-
-tup = tuple('test_string')
-tup
-
-nested_tup = (4, 5, 6), (7, 8)
-print(nested_tup)
-# Lists are standard, that's what every other script is called
-
-dictionary = {"a": "some value", 
-              "b": [1, 2, 3, 4]}
-
-
 df = pd.read_csv("data/fdt_creativity.csv")
+
+df_new = df.copy()
+
+## Initial attempts at scoring
+
+df['dt_Achiev'] = df.filter(like='dt_Achiev_').mean(axis=1, skipna=True)
+
+df['dt_Achiev']
+
+reverse_names = [col for col in df.columns if "rev" in col]
+
+df['dt_Activ_01rev']
+
+for item in reverse_names:
+    df[item] = 6 - df[item]
