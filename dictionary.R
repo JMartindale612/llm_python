cf59ebfaa6a82ae7f31ec55a4a7658491ca2eb31
John Martindale <jmartind@ed.ac.uk>
2025-04-04 20:23:05 +0100

    DONALD first analysis

---
+++ dictionary.R
@@ -0,0 +1,43 @@
+# dictionary 
+
+restorative_nostalgia_dict <- dictionary(list(
+  restorative_nostalgia = c(
+    # Historical framing
+    "earlier", "once was", "ancestor", "heritage",
+    "legacy", "roots", "traditional", "foundation", "forefather",
+    "before", "tradition",  "proud", "values",
+    "pride", "past", "now", "changes", "used to be", "was",
+    "young", "youth", "legacy", "ancestral", "yesterday",
+    
+    # Idealization/Positive Sentiment About the Past
+    "golden", "glorious", "idyllic", "rose-tinted", "home",
+    "prosperity", "cherished", "storied", "lost glory", "once proud",
+    "better", "authentic", "peaceful", "pleasant", "real country",
+    "greatness", "impressive", "lost paradise",
+    
+    # Negativity About the Present or Future
+    "decline", "decay", "losing", "lost", "eroding", "erosion", "moral",
+    "falling apart", "unravel", "broken", "astray",
+    "worse", "tomorrow", "future", "bittersweet",
+    
+    # Superiority / group membership
+    "our people", "our", "us", "them", "their", "patriots", 
+    "group", "we", "real", "outsiders", "majority", "minority", "belong",
+    "native", "superior", "inferior",
+    
+    # Calls to Restore or Revive the Past
+    "bring back", "restore", "reclaim", "revive", "take back",
+    "great again", "return", "renaissance", "reset", "security", "fix",
+    "recapture",
+    
+    # Terms associated with identity fusion and extremism
+    "homeland", "motherland", "fatherland", "family", "loyalty",
+    "blood", "brother", "sister", "daughters", "sons", "conspirators",
+    "stealing", "stolen", "resistance", "glory", "admire", "bloodline",
+    "kin", "traitor", "demolish", "pollute", "scum", "impure", 
+    "brainwashed", "subjugate", "overwhelmed", "under siege", "running out of time",
+    "betray", "betrayal", "sell", "sold", "conspire", "collude",
+    # Martyrdom
+    "defend", "protect", "fight", "self-defence", "self-defense", "preserve"
+  )
+))
